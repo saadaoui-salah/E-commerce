895e8d00bbf4a549836ba4c57bbbfaf752678eeb
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-04-23 15:09:05 -0700

    update vendors page

--- front-end/src/pages/Users/Vendors.js
+++ front-end/src/pages/Users/Vendors.js
@@ -1,12 +1,50 @@
+import Table from '../../components/Table'
+import { TableOptions } from '../../components/sub-components/Buttons'
+import { OptionsReducer } from '../../reducers/reducers'
+import {initialState} from '../../reducers/state'
+import {useReducer } from 'react'
+import {OptionsStateContext, OptionsDispatchContext} from '../../reducers/context'
+
+const columns = ["username", "Total Orders", "Country", "Confirmed", "Days Left to Pay"];
+
+function createData(username, total_orders, total_expense, country, days) {
+    return [username, total_orders, total_expense, country, days];
+}
+
+const rows = [
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+    createData('India', 'IN', 1324171354, 3287263, "user"),
+];
 
 
 
+export default function Users() {
 
-
-
-export default function Users(){
-    return(
-        <>
-        </>
-        )
+    const [options, dispatch] = useReducer(OptionsReducer,initialState.options)
+    const options_ = { name: "Options", component: (id) => <TableOptions id={id} /> }
+    return (
+        <OptionsStateContext.Provider value={options}>
+            <OptionsDispatchContext.Provider value={dispatch}>
+                <Table columns={columns} rows={rows} options={options_} />
+            </OptionsDispatchContext.Provider>
+        </OptionsStateContext.Provider >
+    )
 }
