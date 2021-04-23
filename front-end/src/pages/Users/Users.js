f32fe57892c5b8355f85b42f318030b2088815c4
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-04-23 15:23:28 -0700

    create devices table

--- front-end/src/pages/Users/Users.js
+++ front-end/src/pages/Users/Users.js
@@ -1,10 +1,33 @@
+import Table from '../../components/Table'
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
+import TabletIcon from '@material-ui/icons/Tablet';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 
+const columns = ["Contries", <DesktopWindowsIcon/>, <TabletIcon/>, <PhoneIphoneIcon/>]
 
+function createData(contry, desktop, tablet, phone){
+    return [contry, desktop, tablet, phone]
+}
 
+const rows = [
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+    createData("Algeria", "20%", "30%", "80%"),
+]
 
 export default function Users(){
     return(
         <>
+        <Table rows={rows} columns={columns} />
         </>
         )
 }
