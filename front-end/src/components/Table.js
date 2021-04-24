34dee732a4c133a853f0d38dd8cee3921e7f5367
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-04-23 17:07:29 -0700

    add pagination

--- front-end/src/components/Table.js
+++ front-end/src/components/Table.js
@@ -6,9 +6,9 @@ import {
     TableHead,
     Paper,
     TableContainer,
+    TablePagination
 } from "@material-ui/core";
 import { useState } from 'react'
-import { Options } from '../components/sub-components/Buttons'
 import { EditField } from '../components/sub-components/CustomTextField'
 
 const Header = ({ columns }) => {
@@ -33,54 +33,67 @@ const Header = ({ columns }) => {
 const Body = (props) => {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
-
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
-
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
-
     return (
-        <TableBody>
-            {props.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, rIndex) => {
-                return (
-                    <TableRow key={rIndex} hover role="checkbox" tabIndex={-1} >
-                        {props.columns.map((column, index) => {
-                            const value = row[index];
-                            if (props.options) {
-                                if (column === props.options.name) {
+        <>
+            <TableBody>
+                {props.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, rIndex) => {
+                    return (
+                        <TableRow key={rIndex} hover role="checkbox" tabIndex={-1} >
+                            {props.columns.map((column, index) => {
+                                const value = row[index];
+                                if (props.options) {
+                                    if (column === props.options.name) {
+                                        return (
+                                            <TableCell key={index} align="center">
+                                                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                                    {props.options.component(rIndex)}
+                                                </div>
+                                            </TableCell>
+                                        )
+                                    }
                                     return (
-                                        <TableCell key={index} align="center">
-                                            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                                {props.options.component(rIndex)}
-                                            </div>
+                                        <TableCell key={column.id} align="center">
+                                            <EditField id={rIndex} value={value} />
                                         </TableCell>
-                                    )
+                                    );
                                 }
                                 return (
                                     <TableCell key={column.id} align="center">
-                                        <EditField id={rIndex} value={value} />
+                                        {value}
                                     </TableCell>
-                                );
-                            }
-                            return (
-                                <TableCell key={column.id} align="center">
-                                    {value}
-                                </TableCell>
-                            )
-                        })}
-                    </TableRow>
-                );
-            })}
-        </TableBody>
+                                )
+                            })}
+                        </TableRow>
+                    );
+                })}
+
+            </TableBody>
+            
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 15, 20, 25, 30]}
+                    component="div"
+                    count={props.rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            
+
+        </>
     )
 }
 
 
 export default function CustomTable({ rows, columns, options }) {
+
     return (
         <>
             <Paper>
@@ -91,7 +104,6 @@ export default function CustomTable({ rows, columns, options }) {
                     </Table>
                 </TableContainer>
             </Paper>
-
         </>
     )
 }
