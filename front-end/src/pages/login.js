24115f2c466d4a442ee22142d69ae3b8b9eee29f
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-04-30 18:20:17 -0700

    create login page

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,13 +1,37 @@
-import { Grid, Paper } from "@material-ui/core"
-
+import { Grid, TextField, Paper, Button } from "@material-ui/core"
 
 
 
 export const Login = () => {
-    return(
+    return (
         <Grid container justify="center" align="center">
-            <Paper elevation={3}>
-                lhj
+            <Paper
+                style={{ backgroundColor: "#101b38" }}
+                elevation={3}>
+                <div
+                    style={{ display: 'flex' }}
+                >
+                    <div>
+                        <h1 style={{color:'white'}}>Welcome !</h1>
+                        <h3 style={{color:'white'}}>Sign In</h3>
+                        <TextField
+                            variant="outlined"
+                            label="Email"
+                        />
+                        <br />
+                        <TextField
+                            
+                            variant="outlined"
+                            label="Password"
+                        />
+                        <br />
+                        <Button
+                            variant="contained"
+                            color="success.main"
+                        >Login</Button>
+                    </div>
+                    <div></div>
+                </div>
             </Paper>
         </Grid>
     )
