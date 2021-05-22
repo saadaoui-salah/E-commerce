aec3378e69aec1e9fd4d3f4e476b380a75a1edd8
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-22 10:54:37 -0700

    Delete: console.log

--- front-end/src/reducers/reducers.js
+++ front-end/src/reducers/reducers.js
@@ -40,7 +40,6 @@ export const Dropdown = (state, action) => {
 }
 
 export const productReducer = (state, action) => {
-    console.log(state)
     switch (action.type) {
         case ADD_PRODUCT:
             return [...state, {
