9bcdbcce16bf08ea67c94e613ea498738f64ba53
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-23 04:23:14 -0700

    create useSelect hook

--- front-end/src/hooks.js
+++ front-end/src/hooks.js
@@ -1,6 +1,14 @@
 import {useState} from 'react'
 
-const useToggle = (initialValue) => {
+export const useToggle = (initialValue) => {
     const [toggle, setToggle] = useState(initialValue)
     return toggle, setToggle(!toggle)
+}
+
+export const useSelect = (initialValue)=>{
+    const [selected, setSelected] = useState(initialValue)
+    function handleChange(e){
+        setSelected(e.target.value)
+    }
+    return selected, handleChange
 }
