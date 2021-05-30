d1f6b49cd693ef86e2eeec08fabb55e4928d9fab
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-30 03:21:48 -0700

    add export for theme

--- front-end/src/themes.js
+++ front-end/src/themes.js
@@ -1,7 +1,7 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
 
-const theme = createMuiTheme({
+export const theme = createMuiTheme({
     type: 'dark',
     palette: {
         primary: {
