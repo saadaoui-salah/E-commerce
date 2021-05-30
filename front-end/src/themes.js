c7a5cfbcb0df05bb2a6207df15c9b76b8c91b489
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-30 03:18:00 -0700

    add theme

--- front-end/src/themes.js
+++ front-end/src/themes.js
@@ -1,1 +1,18 @@
 import { createMuiTheme } from '@material-ui/core/styles'
+
+
+const theme = createMuiTheme({
+    type: 'dark',
+    palette: {
+        primary: {
+            light: '#fff',
+            main: '#ff',
+            dark: '#101b38',
+            contrastText: {
+                active: '#0097ff',
+                desactive: '#7bc3f5b5',
+                normal: '#fff'
+            }
+        },
+    }
+})
