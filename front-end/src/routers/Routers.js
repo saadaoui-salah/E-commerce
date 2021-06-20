9817a7a7bb5b5470eca81deeb0f85009b6605bdc
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-06-20 08:13:57 -0700

    add background color to sub routers

--- front-end/src/routers/Routers.js
+++ front-end/src/routers/Routers.js
@@ -3,59 +3,66 @@ import {
     Route,
     Link,
 } from 'react-router-dom'
-import { Grid } from '@material-ui/core'
+import { Grid, Paper } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(() => ({
     link: {
         textDecoration: 'none',
         fontWeight: 'bold',
-        color: "#888",
+        color: "#fff",
         marginLeft: '5px',
         '&:hover': {
             transition: '0.3s',
-            color: "#fff"
+            color: "#0af"
         }
     },
     slash: {
-        marginLeft:'5px',
-        color: '#888',
+        marginLeft: '5px',
+        color: '#f1f1f1',
         fontWeight: 'bold'
+    },
+    active: {
+        color: "#0af"
     }
+
 }))
 
-export default function Routers({ link,data }) {
+export default function Routers({ link, data }) {
     const style = useStyles()
     return (
-        <>
-            <Grid container justify="flex-start">
-                {
-                    data.items.map(item => {
+        <Grid>
+            <Paper style={{ backgroundColor: "#4c5364", marginBottom: "10px" }}>
+                <Grid container justify="flex-start">
+                    {
+                        data.items.map(item => {
+                            return (
+                                <Link
+                                    style={{ textDecoration: 'none' }}
+                                    key={item.id}
+                                    to={`/${link}${item.to}`}>
+                                    <div style={{ display: 'flex' }}>
+                                        <p className={style.link}>{item.enName}</p>
+                                        <p className={style.slash}>/ </p>
+                                    </div>
+                                </Link>
+                            )
+
+                        })
+                    }
+                </Grid>
+            </Paper>
+            <Paper style={{ backgroundColor: "grey" }}>
+                <Switch>
+                    {data.pages.map(page => {
                         return (
-                            <Link
-                                style={{ textDecoration: 'none' }}
-                                key={item.id}
-                                to={`/${link}${item.to}`}>
-                                <div style={{ display: 'flex' }}>
-                                    <p className={style.link}>{item.enName}</p>
-                                    <p className={style.slash}>/ </p>
-                                </div>
-                            </Link>
+                            <Route key={page.id} path={`/${link}/${page.path}`} component={page.component} />
                         )
-
-                    })
-                }
-
-            </Grid>
-            <Switch>
-                {data.pages.map(page => {
-                    return (
-                        <Route key={page.id} path={`/${link}/${page.path}`} component={page.component} />
+                    }
                     )
-                }
-                )
-                }
-            </Switch>
-        </>
+                    }
+                </Switch>
+            </Paper>
+        </Grid>
     )
 }
