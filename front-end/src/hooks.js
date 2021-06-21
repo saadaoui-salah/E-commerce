6db7685610ecfc105a8e5ee34156ec41a5cd6d27
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-06-21 11:51:41 -0700

    create useForm hook

--- front-end/src/hooks.js
+++ front-end/src/hooks.js
@@ -1,21 +1,16 @@
 import { useState } from "react";
 
 
-export const useForm = (callback, initialState= {} ) => {
+export const useForm = (callback, initialState = {}) => {
     const [values, setValues] = useState(initialState)
 
     const onChange = (e) => {
-        setValues({...values, [e.target.name]:[e.target.value]})
+        setValues({ ...values, [e.target.name]: [e.target.value] })
     }
 
     const onSubmit = (e) => {
         e.preventDefault()
         callback()
     }
-
-    return {
-        values, 
-        onChange,
-        onSubmit
-    }
+    return { values, onChange, onSubmit,}
 }
