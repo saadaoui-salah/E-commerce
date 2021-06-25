bde28749ebf04e41a8c44d971d719f6e15071b75
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-06-25 08:51:13 -0700

    add dark mode

--- front-end/src/pages/Product/Products.js
+++ front-end/src/pages/Product/Products.js
@@ -1,21 +1,23 @@
 import Table from '../../components/Table'
 import { AddButton, TableOptions, DeleteButton } from '../../components/sub-components/Buttons'
 import { ProductFrom } from '../../components/CustomForms'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { LOAD_PRODUCTS } from '../../graphql/queries'
 import { Typography, Container, Divider } from '@material-ui/core'
+import { DarkContext } from '../../reducers/context'
 
 const columns = ["Image", "Product", "Category", "Quantity", "Price", "Benifits"];
 function createData(image, product, category, parentCategory, quantity, bPrice, vPrice) {
     const benifits = parseFloat(vPrice) - parseFloat(bPrice);
     category = `${parentCategory.category} / ${category.category}`
-    return [image, product, category, quantity, vPrice, parseFloat(benifits)];
+    return [image, product, category, quantity, vPrice, benifits.toFixed(2)];
 }
 
 
 
 export default function Products() {
+    const { state } = useContext(DarkContext)
     const [products, setProducts] = useState([])
     const options_ = { name: "Options", component: (id) => <TableOptions id={id} /> }
     const { error, laoding, data } = useQuery(LOAD_PRODUCTS)
@@ -32,14 +34,14 @@ export default function Products() {
             product.category,
             product.parentCategory,
             product.quantity,
-            product.bPrice,
-            product.vPrice
+            product.priceAchat,
+            product.priceVender
         )]
     })
     return (
-        <Container amxWidth="lg">
-            <Typography variant="h4" style={{ color: "#fff",  paddingTop:"20px" }}>Manage Your Products</Typography>
-            <Divider style={{marginTop:"10px", marginBottom:"30px", backgroundColor:"#f1f1f1" }} />
+        <Container maxWidth="lg">
+            <Typography variant="h4" style={{ color: state ? "#fff" : "#000",  paddingTop:"20px" }}>Manage Your Products</Typography>
+            <Divider style={{marginTop:"10px", marginBottom:"30px", backgroundColor: state ? "#f1f1f1" : "#010101" }} />
             <div style={{ marginBottom: "10px" }}>
                 <AddButton value="Product" content={<ProductFrom />} title="Create New Product" style={{marginRight:"100px"}} />
                 <DeleteButton content="Are you sure" title="Delete Product" />
