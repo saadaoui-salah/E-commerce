536d022ff5333dd880cd96a27fdd2cb66a9655c3
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-06-28 10:51:04 -0700

    delete previous tests

--- Back-end/product/test/__init__.py
+++ Back-end/product/test/__init__.py
@@ -1,24 +1,8 @@
-from .models import ModelsTest
-from .managers import ManagersTest
-from .qeuries import ProductAppQueryTest
+from .models import CategoryTest
 
-def test_models(category,product):
-    test = ModelsTest()
-    test.category_model_test(category=category)
-    test.product_model_test(product=product)
-    test.product_methods_test()
-    test.product_info_model_test()
-    test.cobon_model_test()
 
-def test_managers():
-    test = ManagersTest()
-    test.test_product_managers()
-    test.setup()
-    test.run_test()
+def test_models():
+    test = CategoryTest()
+    test.category_model_test()
 
-#test_models("choese","adidas choese")
-#test_managers()
-
-test = ProductAppQueryTest()
-test.categroy_query_test()
-test.product_info_query_test()
+test_models()
