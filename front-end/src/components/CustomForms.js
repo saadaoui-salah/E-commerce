b43b2c7daf8d81dba0d0d50f661ca46b831b476d
saadaoui-salah <salahsaadaoui8@gmail.com>
2021-07-13 16:36:42 -0700

    add image input

--- front-end/src/components/CustomForms.js
+++ front-end/src/components/CustomForms.js
@@ -13,15 +13,13 @@ import { useForm } from '../hooks'
 
 
 export function ProductFrom() {
-    const [ categoriesState, setCategories ] = useState([])
-    const [ parentCategoriesState, setParentCategories ] = useState([])
-    
-    const {  loading, data } = useQuery(LOAD_PARENT_CATEGORIES)
+    const [categoriesState, setCategories] = useState([])
+    const [parentCategoriesState, setParentCategories] = useState([])
+
+    const { loading, data } = useQuery(LOAD_PARENT_CATEGORIES)
     const [getCategories, response] = useLazyQuery(LOAD_CATEGORIES)
-    
+
     const { values, onChange, onSubmit } = useForm(createProduct, {
-        id: 61,
-        vendor: 5,
         name: '',
         parentCategory: '',
         category: '',
@@ -44,115 +42,125 @@ export function ProductFrom() {
             getCategories({ variables: { id: values.parentCategory.id } })
         }
         if (response.data !== undefined) {
+            console.log(response.data.getCategories)
             setCategories(response.data.getCategories)
         }
     }, [values.parentCategory, response.data])
     return (
         <>
-            <Grid container spacing={2} direction="row">
-                <Grid item>
-                    <Grid container spacing={2} justify="center" align="center" direction="column">
-                        <Grid item>
-                            <TextField
-                                onChange={(e) => onChange(e)}
-                                required
-                                fullWidth
-                                autoFocus
-                                label="Name"
-                                name="name"
-                                variant="outlined"
+            <Grid container spacing={2} direction="column">
+                <Grid container spacing={2} direction="row">
+                    <Grid item>
+                        <Grid container spacing={2} justify="center" align="center" direction="column">
+                            <Grid item>
+                                <TextField
+                                    onChange={(e) => onChange(e)}
+                                    required
+                                    fullWidth
+                                    autoFocus
+                                    label="Name"
+                                    name="name"
+                                    variant="outlined"
                                 />
-                        </Grid>
-                        <Grid item>
-                            <TextField
-                                onChange={(e) => onChange(e)}
-                                style={{ width: '100%' }}
-                                select
-                                name="parentCategory"
-                                fullWidth
-                                variant="outlined"
-                                label="Parent Category">
-                                { parentCategoriesState.length > 0 ? 
-                                parentCategoriesState.map(category => {
-                                    return (
-                                        <MenuItem value={category.id} key={category.id}>{category.category}</MenuItem>
-                                        )
-                                    }) : 
-                                    <MenuItem value="Nothing to select">"Nothing to select"</MenuItem>
-                                }
-                            </TextField>
-                        </Grid>
-                        <Grid item>
-                            <TextField
-                                style={{ width: '100%' }}
-                                select
-                                fullWidth
-                                name="category"
-                                onChange={(e) => onChange(e)}
-                                label="Category"
-                                variant="outlined"
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    onChange={(e) => onChange(e)}
+                                    style={{ width: '100%' }}
+                                    select
+                                    name="parentCategory"
+                                    fullWidth
+                                    variant="outlined"
+                                    label="Parent Category">
+                                    {parentCategoriesState.length > 0 ?
+                                        parentCategoriesState.map(category => {
+                                            return (
+                                                <MenuItem value={category} key={category.id}>{category.category}</MenuItem>
+                                            )
+                                        }) :
+                                        <MenuItem value="Nothing to select">"Nothing to select"</MenuItem>
+                                    }
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    style={{ width: '100%' }}
+                                    select
+                                    fullWidth
+                                    name="category"
+                                    onChange={(e) => onChange(e)}
+                                    label="Category"
+                                    variant="outlined"
                                 >
-                                {categoriesState.length > 0 ? 
-                                categoriesState.map(category => {
-                                    return (
-                                        <MenuItem value={category.id} key={category.id}>{category.category}</MenuItem>
-                                        )
-                                    })
-                                    : 
-                                    <MenuItem value="Nothing to select">"Nothing to select"</MenuItem>
-                                }
-                            </TextField>
+                                    {categoriesState.length > 0 ?
+                                        categoriesState.map(category => {
+                                            return (
+                                                <MenuItem value={category} key={category.id}>{category.category}</MenuItem>
+                                            )
+                                        })
+                                        :
+                                        <MenuItem value="Nothing to select"> Nothing to select</MenuItem>
+                                    }
+                                </TextField>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Grid container spacing={2} justify="center" align="center" direction="column">
+                            <Grid item>
+                                <TextField
+                                    onChange={(e) => onChange(e)}
+                                    label="Quantity"
+                                    fullWidth
+                                    name="quantity"
+                                    type="number"
+                                    required
+                                    variant="outlined"
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    onChange={(e) => onChange(e)}
+                                    label="Buy Price"
+                                    name="priceAchat"
+                                    fullWidth
+                                    type="number"
+                                    variant="outlined"
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    onChange={(e) => onChange(e)}
+                                    name="priceVendre"
+                                    fullWidth
+                                    label="Vendre"
+                                    required
+                                    type="number"
+                                    variant="outlined"
+                                />
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item>
-                    <Grid container spacing={2} justify="center" align="center" direction="column">
-                        <Grid item>
-                            <TextField
-                                onChange={(e) => onChange(e)}
-                                label="Quantity"
-                                fullWidth
-                                name="quantity"
-                                type="number"
-                                required
-                                variant="outlined"
-                                />
-                        </Grid>
-                        <Grid item>
-                            <TextField
-                                onChange={(e) => onChange(e)}
-                                label="Buy Price"
-                                name="priceAchat"
-                                fullWidth
-                                type="number"
-                                variant="outlined"
-                                />
-                        </Grid>
-                        <Grid item>
-                            <TextField
-                                onChange={(e) => onChange(e)}
-                                name="priceVendre"
-                                fullWidth
-                                label="Vendre"
-                                required
-                                type="number"
-                                variant="outlined"
-                                />
-                        </Grid>
-                    </Grid>
+                <Grid container spacing={0} direction="row">
+                    <TextField
+                        fullWidth
+                        style={{ marginTop: "10px" }}
+                        rows={10}
+                        onChange={e => onChange(e)}
+                        name="detail"
+                        label="Details"
+                        type="text"
+                        multiline={true}
+                        variant="outlined" />
                 </Grid>
             </Grid>
-            <Grid container spacing={0} direction="row">
-                <TextField
-                    fullWidth
-                    style={{marginTop:"10px"}}
-                    rows={10}
-                    onChange={e => onChange(e)}
-                    name="detail"
-                    label="Details"
-                    type="text"
-                    multiline={true}
-                    variant="outlined"/>
+            <Grid container spacing={0} direction="column">
+               {/*  <input 
+                    type="file"
+                    name="image"
+                    onChange={e =>{ if onChange(e)}}
+                    /> */}
             </Grid>
             <Button onClick={(e) => onSubmit(e)}>ADD</Button>
         </>
@@ -164,7 +172,7 @@ export function GlobalCategoryForm() {
         category: ''
     })
     const [addCategory, response] = useMutation(ADD_CATEGORY, { variales: values })
-    function createCategory(){
+    function createCategory() {
         addCategory()
     }
     return (
@@ -176,7 +184,7 @@ export function GlobalCategoryForm() {
                     label={"Sub Category"}
                     variant="outlined"
                     value={values.category}
-                    />
+                />
             </Grid>
         </Grid>
     )
@@ -184,24 +192,24 @@ export function GlobalCategoryForm() {
 
 
 export function SubCategoryForm() {
-    const {  error, loading, data } = useQuery(LOAD_PARENT_CATEGORIES)
+    const { error, loading, data } = useQuery(LOAD_PARENT_CATEGORIES)
     const [categories, setCategories] = useState([])
     useEffect(() => {
-        if ( !loading && data !== undefined) {
+        if (!loading && data !== undefined) {
             setCategories(data.getCategories)
             console.log(categories)
         }
-    }, [loading, data]) 
+    }, [loading, data])
     const { values, onChange, onSubmit } = useForm(createCategory, {
         parentCategories: 2,
         category: 'from react'
     })
-        const [addCategory, response] = useMutation(ADD_CATEGORY, { variales: { category: "from react" } })
-        function createCategory(){
-            addCategory()
-        }
-        return (
-            <Grid container direction="row" justify="space-evenly" align="center" spacing={2}>
+    const [addCategory, response] = useMutation(ADD_CATEGORY, { variales: { category: "from react" } })
+    function createCategory() {
+        addCategory()
+    }
+    return (
+        <Grid container direction="row" justify="space-evenly" align="center" spacing={2}>
             <Grid item xs={12} md={3} >
                 <TextField
                     select
@@ -211,13 +219,13 @@ export function SubCategoryForm() {
                     label="Category"
                     style={{ width: '100%' }}
                     variant="outlined"
-                    >{
+                >{
                         categories.length > 0 ?
-                        categories.map(category => {
-                            <MenuItem key={category.id} value={category.id}>{category.category}</MenuItem>
-                        
-                        }) :
-                        <MenuItem > Nothing to select </MenuItem>
+                            categories.map(category => {
+                                <MenuItem key={category.id} value={category.id}>{category.category}</MenuItem>
+
+                            }) :
+                            <MenuItem > Nothing to select </MenuItem>
                     }
                 </TextField>
             </Grid>
